ab9711fb5561a6891b99764a10af0cad78c3c9ce
Humberto Gómez <humberto.gomez@basetis.com>
2017-02-12 11:51:11 +0100

    Fixes to pick selection

--- client/controllers/pick.js
+++ client/controllers/pick.js
@@ -31,58 +31,65 @@ angular.module('Music-Dictators').controller('pickSelectionCtrl', function($scop
       }
     ],
     picks:
-      [
-        {
-          "name": "Rajoy",
-          "url": "img/poli/rajoy.jpg",
-          "pickId": 7,enabled: true,
-          "powerName": "Skip song",
-          "powerDescription": "Plays the next song"
-        },
-        {
-          "name": "Merkel",
-          "url": "img/poli/merkel.jpg",
-          "pickId": 8,enabled: true,
-          "powerName": "Loan",
-          "powerDescription": "Moves the ball to you by a certain distance and after 5 seconds it moves it double the distant into opponents direction"
-        },
-        {
-          "name": "Trump",
-          "url": "img/poli/trump.jpg",
-          "pickId": 9,enabled: true,
-          "powerName": "Wall",
-          "powerDescription": "Ball changes direction with -2 to it's speed"
-        },
-        {
-          "name": "Hitler",
-          "url": "img/poli/hitler.jpeg",
-          "pickId": 10,enabled: true,
-          "powerName": "Torture",
-          "powerDescription": "Responses are now visible to everyone"
-        },
-        {
-          "name": "Sarkozy",
-          "url": "img/poli/sarkozy.jpg",
-          "pickId": 11,enabled: true,
-          "powerName": "Mute song",
-          "powerDescription": "Stops the audio of the current song for 5 seconds"
-        },
-        {
-          "name": "Bush",
-          "url": "img/poli/bush.jpg",
-          "pickId": 12,enabled: true,
-          "powerName": "Libreation",
-          "powerDescription": "Copies a random ultimate from and opponent"
-        }
-      ]
+      {
+
+      }
   };
   // start socket
   socket.con();
 
+
+  socket.on('loadPicks', function(picks) {
+    $scope.picks = [
+      {
+        "name": "Rajoy",
+        "url": "img/poli/rajoy.jpg",
+        "pickId": 7,enabled: true,
+        "powerName": "Skip song",
+        "powerDescription": "Plays the next song"
+      },
+      {
+        "name": "Merkel",
+        "url": "img/poli/merkel.jpg",
+        "pickId": 8,enabled: true,
+        "powerName": "Loan",
+        "powerDescription": "Moves the ball to you by a certain distance and after 5 seconds it moves it double the distant into opponents direction"
+      },
+      {
+        "name": "Trump",
+        "url": "img/poli/trump.jpg",
+        "pickId": 9,enabled: true,
+        "powerName": "Wall",
+        "powerDescription": "Ball changes direction with -2 to it's speed"
+      },
+      {
+        "name": "Hitler",
+        "url": "img/poli/hitler.jpeg",
+        "pickId": 10,enabled: true,
+        "powerName": "Torture",
+        "powerDescription": "Responses are now visible to everyone"
+      },
+      {
+        "name": "Sarkozy",
+        "url": "img/poli/sarkozy.jpg",
+        "pickId": 11,enabled: true,
+        "powerName": "Mute song",
+        "powerDescription": "Stops the audio of the current song for 5 seconds"
+      },
+      {
+        "name": "Bush",
+        "url": "img/poli/bush.jpg",
+        "pickId": 12,enabled: true,
+        "powerName": "Libreation",
+        "powerDescription": "Copies a random ultimate from and opponent"
+      }
+    ]
+  });
+
   $scope.pick = function(pick) {
     if (!pick.enabled) return false;
 
-    socket.emit('pickDone');
+    socket.emit('pickDone', pick.id);
 
   };
 
@@ -94,10 +101,10 @@ angular.module('Music-Dictators').controller('pickSelectionCtrl', function($scop
       pickId: 'string'
     }
    */
-  socket.on('pickDone ack', function(data) {
+  /*socket.on('pickDone ack', function(data) {
     $scope.portraits[data.team][data.playerId].url = data.url;
     $scope.portraits[data.team][data.playerId].pickId = data.pickId;
-  });
+  });*/
 
   $scope.showDetails = function(item) {
     $scope.details = {
